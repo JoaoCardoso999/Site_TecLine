fcc39b69da50d59b7df1c6d454c469555cbaa44d
JoaoCardoso999 <joaovictorcardoso200508@gmail.com>
2025-10-09 17:55:32 -0300

    Att

---
+++ JS/teste.js
@@ -0,0 +1,56 @@
+
+// Variavel
+
+let nomevarial = 1;
+let nomevarial2 = "BORA BILL";
+let nomevarial3 = 2.7;
+let nomevarial4  = true;
+
+var nome = 1;
+
+const nome = "jhessik";
+
+
+let soma = 3+5;
+let subtracao = 5-3;
+let multiplicacao = 3*5;
+let divisao = 12/2;
+
+// Juntar textos 
+
+let primeironome = "jhessik";
+let sobrenome = "leal";
+let Nomecompleto = primeironome + sobrenome;
+
+// Funções
+// Função sem parametro: Que nao recebe dados dentro do ()
+
+
+function imprimirMsg() {
+    console.log("Hello Word!");
+    console.log(primeironome + "Bem Vinda!");
+}
+
+function somarValores(valor1,valor2) {
+    let soma = valor1 + valor2;
+    console.log("O Resultado da soma e "+soma);
+}
+
+let n1 = 15;
+let n2 = 45;
+
+if(n1=10){
+    console.log("Irei a praia!");
+}else{
+    console.log("Fico em casa!");
+}
+
+let carro = {
+cor: "preto",
+placa: "KRE41234",
+modelo: "Fusca",
+KmRodados: "120000",
+som: true,
+Arcondicionado: true,
+
+}
